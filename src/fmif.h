b3e1ad708cdfd22991657216f51550ecdee543e6
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2024-11-26 16:48:58 +0900

    update

--- src/fmif.h
+++
@@ -1,6 +0,0 @@
-#ifndef FMIF_H
-#define FMIF_H
-//	public
-extern void Fmdriver_init( void );
-extern void Fmdriver_sendMidi( unsigned char byteStream );
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,29 @@
    MISO - Pin12
    SCK  - Pin13
  */
+#include <usbh_midi.h>
+#include <usbhub.h>
+#ifdef USBCON
+#define _MIDI_SERIAL_PORT Serial1
+#else
+#define _MIDI_SERIAL_PORT Serial
+#endif
 
+// Set to 1 if you want to wait for the Serial MIDI transmission to complete.
+// For more information, see https://github.com/felis/USB_Host_Shield_2.0/issues/570
+#define ENABLE_MIDI_SERIAL_FLUSH 0
+
+//////////////////////////
+// MIDI Pin assign
+// 2 : GND
+// 4 : +5V(Vcc) with 220ohm
+// 5 : TX
+//////////////////////////
+
+USB Usb;
+USBH_MIDI  Midi(&Usb);
+
+void MIDI_poll();
 
 #include <SPI.h>
 //0 :5V 1:3.3V
@@ -122,9 +144,18 @@ void keyoff(void){
 }
 
 void setup() {
+  _MIDI_SERIAL_PORT.begin(31250);
+
+  if (Usb.Init() == -1) {
+    while (1); //halt
+  }//if (Usb.Init() == -1...
+  delay( 200 );
+
+
   // put your setup code here, to run once:
   pinMode(9,OUTPUT);
   pinMode(10,OUTPUT);
+  /* pinMode(8,OUTPUT); */
   set_ss_pin(HIGH);
  
   SPI.setBitOrder(MSBFIRST);
@@ -138,26 +169,50 @@ void setup() {
 }
 
 void loop() {
+  Usb.Task();
+
+  if ( Midi ) {
+    MIDI_poll();
+  }
+  //delay(1ms) if you want
+  //delayMicroseconds(1000);
+
   // put your main code here, to run repeatedly:
-  keyon(0x14,0x65);
-  delay(500);
-  keyoff();
-  delay(200);
-  keyon(0x1c,0x11);
-  delay(500);
-  keyoff();
-  delay(200);
-  keyon(0x1c,0x42);
-  delay(500);
-  keyoff();
-  delay(200);
-  keyon(0x1c,0x5d);
-  delay(500);
-  keyoff();
-  delay(200);
-  keyon(0x24,0x17);
-  delay(500);
-  keyoff();
-  delay(200);
+  /* keyon(0x14,0x65); */
+  /* delay(500); */
+  /* keyoff(); */
+  /* delay(200); */
+  /* keyon(0x1c,0x11); */
+  /* delay(500); */
+  /* keyoff(); */
+  /* delay(200); */
+  /* keyon(0x1c,0x42); */
+  /* delay(500); */
+  /* keyoff(); */
+  /* delay(200); */
+  /* keyon(0x1c,0x5d); */
+  /* delay(500); */
+  /* keyoff(); */
+  /* delay(200); */
+  /* keyon(0x24,0x17); */
+  /* delay(500); */
+  /* keyoff(); */
+  /* delay(200); */
 }
 
+// Poll USB MIDI Controler and send to serial MIDI
+void MIDI_poll()
+{
+  uint8_t outBuf[ 3 ];
+  uint8_t size;
+
+  do {
+    if ( (size = Midi.RecvData(outBuf)) > 0 ) {
+      //MIDI Output
+      _MIDI_SERIAL_PORT.write(outBuf, size);
+#if ENABLE_MIDI_SERIAL_FLUSH
+      _MIDI_SERIAL_PORT.flush();
+#endif
+    }
+  } while (size > 0);
+}
